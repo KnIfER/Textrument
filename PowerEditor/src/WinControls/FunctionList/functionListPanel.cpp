f9df864d099d23e9451be82f78e1550a3dd18cd6
KnIfER <1445952621@qq.com>
2021-03-08 21:01:53 +0900

    Merge: Split functionList.xml into multifile in "functionList" Folder, p2
    https://github.com/notepad-plus-plus/notepad-plus-plus/commit/c0c070abaf6c8acfac3fe91c777a50b537b240c6
    
    22 Oct 2020

--- PowerEditor/src/WinControls/FunctionList/functionListPanel.cpp
+++ PowerEditor/src/WinControls/FunctionList/functionListPanel.cpp
@@ -483,12 +483,12 @@ void FunctionListPanel::init(HINSTANCE hInst, HWND hPere, ScintillaEditView **pp
 	if (!doLocalConf)
 	{
 		generic_string funcListXmlPath = (NppParameters::getInstance()).getUserPath();
-		PathAppendCompat(funcListXmlPath, TEXT("functionList.xml"));
+		PathAppendCompat(funcListXmlPath, TEXT("functionList"));
 
 		if (!PathFileExists(funcListXmlPath.c_str()))
 		{
 			generic_string funcListDefaultXmlPath = (NppParameters::getInstance()).getNppPath();
-			PathAppendCompat(funcListDefaultXmlPath, TEXT("functionList.xml"));
+			PathAppendCompat(funcListDefaultXmlPath, TEXT("functionList"));
 			if (PathFileExists(funcListDefaultXmlPath.c_str()))
 			{
 				::CopyFile(funcListDefaultXmlPath.c_str(), funcListXmlPath.c_str(), TRUE);
@@ -503,7 +503,7 @@ void FunctionListPanel::init(HINSTANCE hInst, HWND hPere, ScintillaEditView **pp
 	else
 	{
 		generic_string funcListDefaultXmlPath = (NppParameters::getInstance()).getUserPath();
-		PathAppendCompat(funcListDefaultXmlPath, TEXT("functionList.xml"));
+		PathAppendCompat(funcListDefaultXmlPath, TEXT("functionList"));
 		if (PathFileExists(funcListDefaultXmlPath.c_str()))
 		{
 			_funcParserMgr.init(funcListDefaultXmlPath, ppEditView);
