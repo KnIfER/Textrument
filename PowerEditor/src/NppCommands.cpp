6c400e69fa149e0374c4d6f61b4ee26a6c02e9c6
KnIfER <1445952621@qq.com>
2021-04-02 01:46:38 +0900

    Merge: Fix view file in Edge failure issue if name contains spaces

--- PowerEditor/src/NppCommands.cpp
+++ PowerEditor/src/NppCommands.cpp
@@ -1994,6 +1994,7 @@ void Notepad_plus::command(int id)
 
 		case IDM_VIEW_IN_FIREFOX:
 		case IDM_VIEW_IN_CHROME:
+		case IDM_VIEW_IN_EDGE:
 		case IDM_VIEW_IN_IE:
 		{
 			auto currentBuf = _pEditView->getCurrentBuffer();
@@ -2008,6 +2009,10 @@ void Notepad_plus::command(int id)
 				else if (id == IDM_VIEW_IN_CHROME)
 				{
 					appName = TEXT("chrome.exe");
+				}
+				else if (id == IDM_VIEW_IN_EDGE)
+				{
+					appName = TEXT("msedge.exe");
 				}
 				else // if (id == IDM_VIEW_IN_IE)
 				{
@@ -2032,7 +2037,18 @@ void Notepad_plus::command(int id)
 				{
 					::ShellExecute(NULL, TEXT("open"), valData, fullCurrentPath.c_str(), NULL, SW_SHOWNORMAL);
 				}
-				else
+				else if (id == IDM_VIEW_IN_EDGE)
+				{
+					// Try the Legacy version
+
+					// Don't put the quots for Edge, otherwise it doesn't work
+					//fullCurrentPath = TEXT("\"");
+					generic_string fullCurrentPath = currentBuf->getFullPathName();
+					//fullCurrentPath += TEXT("\"");
+
+					::ShellExecute(NULL, TEXT("open"), TEXT("shell:Appsfolder\\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge"), fullCurrentPath.c_str(), NULL, SW_SHOW);
+				} 
+				else 
 				{
 					_nativeLangSpeaker.messageBox("ViewInBrowser",
 						_pPublicInterface->getHSelf(),
@@ -2041,21 +2057,6 @@ void Notepad_plus::command(int id)
 						MB_OK);
 				}
 				::RegCloseKey(hKey2Check);
-			}
-		}
-		break;
-		
-		case IDM_VIEW_IN_EDGE:
-		{
-			auto currentBuf = _pEditView->getCurrentBuffer();
-			if (!currentBuf->isUntitled())
-			{
-				// Don't put the quots for Edge, otherwise it doesn't work
-				//fullCurrentPath = TEXT("\"");
-				generic_string fullCurrentPath = currentBuf->getFullPathName();
-				//fullCurrentPath += TEXT("\"");
-
-				ShellExecute(NULL, TEXT("open"), TEXT("shell:Appsfolder\\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge"), fullCurrentPath.c_str(), NULL, SW_SHOW);
 			}
 		}
 		break;
