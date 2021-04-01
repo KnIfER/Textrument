e2c4e069d556405a38176c40e22472070c00d8fa
KnIfER <1445952621@qq.com>
2021-04-01 12:29:49 +0900

    Merge: ~Fix scroll to last line problem after main window resize

--- PowerEditor/src/ScitillaComponent/ScintillaEditView.cpp
+++ PowerEditor/src/ScitillaComponent/ScintillaEditView.cpp
@@ -3337,6 +3337,7 @@ void ScintillaEditView::scrollPosToCenter(size_t pos)
 		middleLine = lastVisibleDocLine -  nbLine/2;
 	int nbLines2scroll =  line - middleLine;
 	scroll(0, nbLines2scroll);
+	execute(SCI_ENSUREVISIBLEENFORCEPOLICY, line);
 }
 
 void ScintillaEditView::hideLines()
