690e862a2d97131e6aacb0dfa159a02b3143796b
KnIfER <1445952621@qq.com>
2021-04-09 18:09:56 +0900

    Add another log method(  3=log in the debugger ), allow logging multi-byte strings

--- PowerEditor/src/WinControls/InsituDebug.cpp
+++ PowerEditor/src/WinControls/InsituDebug.cpp
@@ -1,6 +1,7 @@
 
 #include "InsituDebug.h"
 #include "Notepad_plus_msgs.h"
+#include <iostream>
 #include <stdarg.h>
 
 extern HWND mainAppWnd;
@@ -29,6 +30,11 @@ void LogIs(int show, HWND hWnd, const TCHAR* msg, va_list & args)
 		{
 			::MessageBox(hWnd, buffer, TEXT(""), MB_OK);
 		}
+		else if (show==3)
+		{
+			::OutputDebugString(buffer);
+			::OutputDebugStringA("\n");
+		}
 	}
 }
 
@@ -45,3 +51,68 @@ void LogIs(int show, const TCHAR* msg, ...)
 	va_start(args, msg);
 	LogIs(show, (HWND)-1, msg, args);
 }
+
+void LogIs(const TCHAR* msg, ...)
+{
+	va_list args = NULL;
+	va_start(args, msg);
+	LogIs(1, (HWND)-1, msg, args);
+}
+
+#if UNICODE
+
+void LogIs(int show, HWND hWnd, const CHAR* msg, va_list & args)
+{
+	if (hWnd==(HWND)-2)
+	{
+		return;
+	}
+	if(show)
+	{
+		if (hWnd==(HWND)-1)
+		{
+			hWnd = mainAppWnd;
+		}
+
+		CHAR buffer[256]={};
+		vsprintf(buffer,msg, args);
+
+		if (show==1)
+		{
+			TCHAR buffer1[256]={};
+			MultiByteToWideChar(CP_ACP, 0, buffer, -1, buffer1, 255);
+			::SendMessage(hWnd, NPPM_SETSTATUSBAR, STATUSBAR_DOC_TYPE, (LPARAM)buffer1);
+		}
+		else if (show==2)
+		{
+			::MessageBoxA(hWnd, buffer, (""), MB_OK);
+		}
+		else if (show==3)
+		{
+			::OutputDebugStringA(buffer);
+			::OutputDebugStringA("\n");
+		}
+	}
+}
+
+void LogIs(int show, HWND hWnd, const CHAR* msg, ...)
+{
+	va_list args = NULL;
+	va_start(args, msg);
+	LogIs(show, hWnd, msg, args);
+}
+
+void LogIs(int show, const CHAR* msg, ...)
+{
+	va_list args = NULL;
+	va_start(args, msg);
+	LogIs(show, (HWND)-1, msg, args);
+}
+
+void LogIs(const CHAR* msg, ...)
+{
+	va_list args = NULL;
+	va_start(args, msg);
+	LogIs(1, (HWND)-1, msg, args);
+}
+#endif
--- PowerEditor/src/WinControls/InsituDebug.h
+++ PowerEditor/src/WinControls/InsituDebug.h
@@ -5,3 +5,13 @@
 void LogIs(int show, HWND tag, const TCHAR* msg, ...);
 
 void LogIs(int show, const TCHAR* msg, ...);
+
+void LogIs(const TCHAR* msg, ...);
+
+#if UNICODE
+void LogIs(int show, HWND tag, const CHAR* msg, ...);
+
+void LogIs(int show, const CHAR* msg, ...);
+
+void LogIs(const CHAR* msg, ...);
+#endif
